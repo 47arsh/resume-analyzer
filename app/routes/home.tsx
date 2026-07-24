0fb9bf6637dc962d5085b66d4251fc4985689b6a
47arsh <arxhx7@gmail.com>
2026-07-24 21:49:19 +0530

    implement navbar , make constants and types directory

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,32 @@
+import { resumes } from "../../constants";
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
+import Navbar from "~/components/Navbar";
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "ResAnalyze" },
+    { name: "description", content: "Smart feedback for your resume using ResAnalyze!" },
   ];
 }
 
 export default function Home() {
-  return <Welcome />;
+  return <main>
+
+      <Navbar />
+
+      <section className="main-section">
+        <div className="page-heading">
+          <h1>Welcome to ResAnalyze</h1>
+          <h2>Track your applications and resume ratings</h2>
+          <p>Get smart feedback for your resume!</p>
+        </div>
+      </section>
+
+      {resumes.map((resume) => (
+        <div key={resume.id}>
+          <h3>{resume.jobTitle}</h3>
+          <p>{resume.companyName}</p>
+        </div>
+      ))}
+  </main>;
 }
